17587bd273b7cb80d0296182ad90cdee394f1a96
jeremycaine <jeremy_caine@uk.ibm.com>
2023-08-29 20:34:30 +0100

    upd serve model

--- serve-model/src/MNISTImageModel.py
+++ serve-model/src/MNISTImageModel.py
@@ -1,38 +1,52 @@
 import os
-import sys
-import tensorflow as tf
-import ibm_cos_utils as cos
-import tempfile
-import pathlib
+
 import ibm_boto3
-from ibm_botocore.client import Config
-from ibm_botocore.exceptions import ClientError
+from ibm_botocore.client import Config, ClientError
 
+import tensorflow as tf
 
-# credentials
+import pathlib
+import tempfile
+
+def log(e):
+    print("{0}\n".format(e))
+
+# start
+log("start")
+
+# Constants for IBM COS values
 COS_API_KEY_ID = os.getenv('COS_API_KEY_ID')
-
 COS_ENDPOINT = os.getenv('COS_ENDPOINT', 'https://s3.eu-gb.cloud-object-storage.appdomain.cloud')
-COS_AUTH_ENDPOINT = os.getenv('COS_AUTH_ENDPOINT', 'https://iam.cloud.ibm.com/identity/token')
-COS_SERVICE_CRN = os.getenv('COS_SERVICE_CRN', 'crn:v1:bluemix:public:cloud-object-storage:global:a/b71ac2564ef0b98f1032d189795994dc:875e3790-53c1-40b0-9943-33b010521174::')
-COS_STORAGE_CLASS = os.getenv('COS_STORAGE_CLASS','eu-gb-smart')
+COS_INSTANCE_CRN = os.getenv('COS_INSTANCE_CRN', 'crn:v1:bluemix:public:cloud-object-storage:global:a/b71ac2564ef0b98f1032d189795994dc:875e3790-53c1-40b0-9943-33b010521174::')
 
 # file vars
 bucket_name = os.getenv('BUCKET_NAME', 'mnist-model')
-keras_file_name = os.getenv('H5_FILE_NAME', 'mnist-model.keras')
+model_name = os.getenv('MODEL_NAME', 'mnist-model')
+model_file_name = os.getenv('MODEL_FILE_NAME', 'mnist-model.keras')
 
-
-# create cloud objec storage connection
-cos_cli = ibm_boto3.client("s3",
+# Create resource
+cos = ibm_boto3.resource("s3",
     ibm_api_key_id=COS_API_KEY_ID,
-    ibm_service_instance_id=COS_SERVICE_CRN,
-    ibm_auth_endpoint=COS_AUTH_ENDPOINT,
+    ibm_service_instance_id=COS_INSTANCE_CRN,
     config=Config(signature_version="oauth"),
     endpoint_url=COS_ENDPOINT
 )
 
-def log(e):
-    print("{0}\n".format(e))
+def get_item(bucket_name, item_name):
+    print("Retrieving item from bucket: {0}, key: {1}".format(bucket_name, item_name))
+    try:
+        # Create a temporary file
+        with tempfile.NamedTemporaryFile() as f:
+            # Get the pathlib.Path object
+            path = pathlib.Path(f.name)
+        # and file name
+        fn = path.name
+        cos.Object(bucket_name, item_name).download_file(fn)
+        return fn
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to retrieve file contents: {0}".format(e))
 
 
 class MNISTImageModel(object):
@@ -46,15 +60,7 @@ class MNISTImageModel(object):
         """
         log("Initializing")
 
-        # Create a temporary file
-        with tempfile.NamedTemporaryFile() as f:
-            # Get the pathlib.Path object
-            path = pathlib.Path(f.name)
-
-        # and file name
-        fn = path.name
-
-        cos_cli.download_file(bucket_name, keras_file_name, fn)  
+        fn = get_item(bucket_name, model_file_name)
         self.model = tf.keras.models.load_model(fn)
 
     def predict(self,data):
@@ -69,4 +75,4 @@ class MNISTImageModel(object):
         log("Predict called - will run identity function")
         return self.model.predict(data) 
 
-log("finished MyModel")
+log("finished MNISTImageModel")
